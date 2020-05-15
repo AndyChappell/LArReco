3e798f3cb5815243d659b47b2cdc4f386b52e2f0
Andrew Chappell <A.Chappell@warwick.ac.uk>
2020-05-15 09:27:47 +0100

    Refactoring. Fix bug when histograms have no entries.

--- validation/ProcessValidation.C
+++ validation/ProcessValidation.C
@@ -21,8 +21,27 @@
 
 typedef std::map<std::string, TH1F*> InteractionToHistogramMap;
 
+/*** USAGE ***
+
+    .L ProcessValidation.C++
+    ProcessValidation("<Name of your validation file>")
+    MergeKeys()
+    PlotCumulative()    // Optionally set argument true if you want logarithmic scale
+    
+    This will produce lots of PDFs in a pdfs folder (which it assumes already exists) in your working directory. The fractional
+    number of events will be cumulative in this case - i.e. the fraction in a bin represents the total fraction of events that
+    have the current purity/completeness/efficiency less than or equal to the bin value.
+    
+    If you want the non-cumulative version, you can run
+    
+    Plot()              // Optionally set argument true if you want logarithmic scale
+    
+    but be sure to run this before any run of PlotCumulative, because both of these functions operate on the global variables populated
+    by MergeKeys(), and PlotCumulative will change these variables.
+*/
 void ProcessValidation(const char* filename)
 {
+    gROOT->SetBatch(1);     // ATTN: Don't switch this off, you'll regret it. Really.
     gROOT->ProcessLine(".L ../LArReco/validation/Validation.C++");
     Parameters parameters;
     parameters.m_applyUbooneFiducialCut = false;
@@ -34,7 +53,7 @@ void ProcessValidation(const char* filename)
     gDirectory->ls("-m");
 }
 
-void PlotPurity(const std::string& interaction)
+void PlotSinglePurity(const std::string& interaction)
 {
     gStyle->SetOptStat(0);
     const std::string str(interaction + "_MUON_Purity");
@@ -55,7 +74,7 @@ void PlotPurity(const std::string& interaction)
     c.Print(filename.c_str());
 }
 
-void PlotCompleteness(const std::string& interaction)
+void PlotSingleCompleteness(const std::string& interaction)
 {
     gStyle->SetOptStat(0);
     const std::string str(interaction + "_MUON_Completeness");
@@ -76,21 +95,87 @@ void PlotCompleteness(const std::string& interaction)
     c.Print(filename.c_str());
 }
 
-void PlotMerge(const InteractionToHistogramMap& map, const std::string& type);
-void PlotMergeCumulative(const InteractionToHistogramMap& map, const std::string& type);
+void AddHistogramToMap(InteractionToHistogramMap& map, const TString& name, const std::string& key)
+{
+    auto iter = map.find(key);
+    if (iter != map.end())
+    {
+        TH1F* current = iter->second;
+        TH1F* addend = static_cast<TH1F*>(gDirectory->Get(name));
+        if (addend->GetEntries() > 0)
+        {
+            current->Scale(current->GetEntries(), "nosw2");
+            addend->Scale(addend->GetEntries(), "nosw2");
+            current->Add(addend);
+            current->Scale(1.f / current->GetEntries(), "nosw2");
+        }
+    }
+    else
+    {
+        TH1F* hist = static_cast<TH1F*>(gDirectory->Get(name));
+        if (hist->GetEntries() > 0)
+            map.emplace(key, hist);
+    }
+}
+
+void Accumulate(InteractionToHistogramMap& map)
+{
+    for (const auto [key, value] : map)
+        for (int i = 2; i <= value->GetNbinsX(); ++i)
+            value->SetBinContent(i, value->GetBinContent(i) + value->GetBinContent(i - 1));
+}
+
+void Format(InteractionToHistogramMap& map, const bool isLog = false)
+{
+    std::map<std::string, int> particleColor;
+    particleColor.emplace("MUON", kMagenta + 2);
+    particleColor.emplace("ELECTRON", kRed + 1);
+    particleColor.emplace("PIPLUS", kGreen + 2);
+    particleColor.emplace("PIMINUS", kCyan + 2);
+    particleColor.emplace("PHOTON1", kOrange);
+    particleColor.emplace("PHOTON2", kOrange + 1);
+    particleColor.emplace("PHOTON3", kOrange + 2);
+    particleColor.emplace("PHOTON4", kOrange + 3);
+    particleColor.emplace("PHOTON5", kOrange + 4);
+    particleColor.emplace("PROTON1", kBlue);
+    particleColor.emplace("PROTON2", kBlue + 2);
+    particleColor.emplace("PROTON3", kBlue - 4);
+    particleColor.emplace("PROTON4", kBlue - 7);
+    particleColor.emplace("PROTON5", kBlue + 4);
+
+    for (const auto [key, value] : map)
+    {
+        const int splitPos = key.find_last_of("_");
+        std::string interaction = key.substr(0, splitPos);
+        std::string particle = key.substr(splitPos + 1);
+        value->SetLineColor(particleColor.find(particle)->second);
+        value->SetLineWidth(2);
+        value->Draw("hist same");
+        if (isLog)
+        {   // Don't set lower limit of zero, or log scale won't work
+            value->GetYaxis()->SetRangeUser(std::numeric_limits<float>::epsilon(), 1);
+        }
+        else
+        {
+            value->GetYaxis()->SetRangeUser(0, 1);
+        }
+    }
+}
+
+// Yes, global variables, but it's much more convenient when running interactively
+InteractionToHistogramMap mapI2P, mapI2C;
 void MergeKeys()
 {
     gStyle->SetOptStat(0);
     TIter next(gDirectory->GetList());
-    std::string reInteraction("\\b(CCQEL\\_MU|CCQEL\\_E|CCRES\\_MU|CCRES\\_E|CCDIS\\_MU|CCDIS\\_E|NCQEL|NCRES|NCDIS|CCCOH|NCCOH|OTHER\\_INTERACTION)");
-    std::string reProtons("(\\_P\\_P\\_P\\_P\\_P\\_|\\_P\\_P\\_P\\_P\\_|\\_P\\_P\\_P\\_|\\_P\\_P\\_|\\_P\\_)?\\_?");
-    std::string rePrimaries("(PIPLUS|PIMINUS|PIZERO|PHOTON|NULL)?\\_?");
-    std::string reParticle("(MUON|ELECTRON|PIPLUS|PIMINUS|PHOTON1|PHOTON2|PHOTON3|PHOTON4|PHOTON5|PROTON1|PROTON2|PROTON3|PROTON4|PROTON5|NULL])?\\_?");
+    std::string reInteraction("\\b(CCQEL\\_MU|CCQEL\\_E|CCRES\\_MU|CCRES\\_E|CCDIS\\_MU|CCDIS\\_E|NCQEL|NCRES|NCDIS|CCCOH|NCCOH|OTHER\\_INTERACTION)\\_");
+    std::string reProtons("(P\\_)*");
+    std::string rePrimaries("(PIPLUS|PIMINUS|PIZERO|PHOTON)?\\_?");
+    std::string reParticle("(MUON|ELECTRON|PIPLUS|PIMINUS|PHOTON1|PHOTON2|PHOTON3|PHOTON4|PHOTON5|PROTON1|PROTON2|PROTON3|PROTON4|PROTON5)?\\_?");
     std::string reMetrics("(Purity|Completeness)\\b");
     std::string reStr(reInteraction + reProtons + rePrimaries + reParticle + reMetrics);
     TPRegexp re(reStr.c_str());
 
-    InteractionToHistogramMap mapI2P, mapI2C;
     while (TObject* obj = next())
     {
         TString name(obj->GetName());
@@ -112,73 +197,23 @@ void MergeKeys()
                 primary = "PIC";
             std::string interactionKey = interaction +
                 (primary.empty() ? "" : "_" + primary) +
-                //(protons.empty() ? "" : (protons == "_P_" ? "_P" : "_NP")) +
                 "_" + particle;
             if (metric == "Purity")
-            {
-                auto iterI2P = mapI2P.find(interactionKey);
-                if (iterI2P != mapI2P.end())
-                {
-                    TH1F* current = iterI2P->second;
-                    current->Scale(current->GetEntries(), "nosw2");
-                    TH1F* addend = static_cast<TH1F*>(gDirectory->Get(name));
-                    addend->Scale(addend->GetEntries(), "nosw2");
-                    current->Add(addend);
-                    current->Scale(1.f / current->GetEntries(), "nosw2");
-                }
-                else
-                {
-                    TH1F* hist = static_cast<TH1F*>(gDirectory->Get(name));
-                    mapI2P.emplace(interactionKey, hist);
-                }
-            }
+                AddHistogramToMap(mapI2P, name, interactionKey);
             else
-            {
-                auto iterI2C = mapI2C.find(interactionKey);
-                if (iterI2C != mapI2C.end())
-                {
-                    TH1F* current = iterI2C->second;
-                    current->Scale(current->GetEntries(), "nosw2");
-                    TH1F* addend = static_cast<TH1F*>(gDirectory->Get(name));
-                    addend->Scale(addend->GetEntries(), "nosw2");
-                    current->Add(addend);
-                    current->Scale(1.f / current->GetEntries(), "nosw2");
-                }
-                else
-                {
-                    TH1F* hist = static_cast<TH1F*>(gDirectory->Get(name));
-                    mapI2C.emplace(interactionKey, hist);
-                }
-            }    
+                AddHistogramToMap(mapI2C, name, interactionKey);
+        }
+        else if (name.Contains("Purity") || name.Contains("Completeness"))
+        {
+            std::cout << "No match for " << name << std::endl;
         }
         delete arr;
     }
-
-    //PlotMerge(mapI2C, "Completeness");
-    //PlotMerge(mapI2P, "Purity");
-    
-    PlotMergeCumulative(mapI2C, "Completeness");
-    PlotMergeCumulative(mapI2P, "Purity");
 }
 
-void PlotMerge(const InteractionToHistogramMap& map, const std::string& type)
+void PlotMerge(InteractionToHistogramMap& map, const std::string& type, const bool isLog = false)
 {
-    std::map<std::string, int> particleColor;
-    particleColor.emplace("MUON", kMagenta + 2);
-    particleColor.emplace("ELECTRON", kRed + 1);
-    particleColor.emplace("PIPLUS", kGreen + 2);
-    particleColor.emplace("PIMINUS", kCyan + 2);
-    particleColor.emplace("PHOTON1", kOrange);
-    particleColor.emplace("PHOTON2", kOrange + 1);
-    particleColor.emplace("PHOTON3", kOrange + 2);
-    particleColor.emplace("PHOTON4", kOrange + 3);
-    particleColor.emplace("PHOTON5", kOrange + 4);
-    particleColor.emplace("PROTON1", kBlue);
-    particleColor.emplace("PROTON2", kBlue + 2);
-    particleColor.emplace("PROTON3", kBlue - 4);
-    particleColor.emplace("PROTON4", kBlue - 7);
-    particleColor.emplace("PROTON5", kBlue + 4);
-
+    Format(map);
     std::map<std::string, TCanvas*> canvases;
     std::map<std::string, TLegend*> legends;
     for (const auto [key, value] : map)
@@ -192,8 +227,6 @@ void PlotMerge(const InteractionToHistogramMap& map, const std::string& type)
             iter->second->cd();
             TLegend* legend = legends.find(interaction)->second;
             legend->AddEntry(value, particle.c_str(), "l");
-            value->SetLineColor(particleColor.find(particle)->second);
-            value->SetLineWidth(2);
             value->Draw("hist same");
             legend->Draw();
         }
@@ -207,12 +240,11 @@ void PlotMerge(const InteractionToHistogramMap& map, const std::string& type)
             legend->SetTextSize(0.03);
             legend->AddEntry(value, particle.c_str(), "l");
             legends.emplace(interaction, legend);
-            value->SetLineColor(particleColor.find(particle)->second);
-            value->SetLineWidth(2);
-            // Don't set lower limit of zero, or log scale won't work
-            value->GetYaxis()->SetRangeUser(std::numeric_limits<float>::epsilon(), 1);
-            canvas->cd();
-            gPad->SetLogy();
+            if (isLog)
+            {   // Don't set lower limit of zero, or log scale won't work
+                canvas->cd();
+                gPad->SetLogy();
+            }
             value->Draw("hist");
             legend->Draw();
         }
@@ -232,77 +264,19 @@ void PlotMerge(const InteractionToHistogramMap& map, const std::string& type)
     legends.clear();
 }
 
-void PlotMergeCumulative(const InteractionToHistogramMap& map, const std::string& type)
+void PlotCumulative(const bool isLog = false)
 {
-    std::map<std::string, int> particleColor;
-    particleColor.emplace("MUON", kMagenta + 2);
-    particleColor.emplace("ELECTRON", kRed + 1);
-    particleColor.emplace("PIPLUS", kGreen + 2);
-    particleColor.emplace("PIMINUS", kCyan + 2);
-    particleColor.emplace("PHOTON1", kOrange);
-    particleColor.emplace("PHOTON2", kOrange + 1);
-    particleColor.emplace("PHOTON3", kOrange + 2);
-    particleColor.emplace("PHOTON4", kOrange + 3);
-    particleColor.emplace("PHOTON5", kOrange + 4);
-    particleColor.emplace("PROTON1", kBlue);
-    particleColor.emplace("PROTON2", kBlue + 2);
-    particleColor.emplace("PROTON3", kBlue - 4);
-    particleColor.emplace("PROTON4", kBlue - 7);
-    particleColor.emplace("PROTON5", kBlue + 4);
-
-    std::map<std::string, TCanvas*> canvases;
-    std::map<std::string, TLegend*> legends;
-    for (const auto [key, value] : map)
-    {
-        const int splitPos = key.find_last_of("_");
-        std::string interaction = key.substr(0, splitPos);
-        std::string particle = key.substr(splitPos + 1);
-        auto iter = canvases.find(interaction);
-        if (iter != canvases.end())
-        {   // Update existing plot
-            iter->second->cd();
-            TLegend* legend = legends.find(interaction)->second;
-            legend->AddEntry(value, particle.c_str(), "l");
-            value->SetLineColor(particleColor.find(particle)->second);
-            value->SetLineWidth(2);
-            for (int i = 2; i <= value->GetNbinsX(); ++i)
-                value->SetBinContent(i, value->GetBinContent(i) + value->GetBinContent(i - 1));
-            value->Draw("hist same");
-            legend->Draw();
-        }
-        else
-        {   // Create new plot
-            TCanvas* canvas = new TCanvas(interaction.c_str(), interaction.c_str(), 1600, 900);
-            canvases.emplace(interaction, canvas);
-            TLegend* legend(new TLegend(0.15, 0.65, 0.4, 0.88));
-            legend->SetFillStyle(0);
-            legend->SetBorderSize(0);
-            legend->SetTextSize(0.03);
-            legend->AddEntry(value, particle.c_str(), "l");
-            legends.emplace(interaction, legend);
-            value->SetLineColor(particleColor.find(particle)->second);
-            value->SetLineWidth(2);
-            for (int i = 2; i <= value->GetNbinsX(); ++i)
-                value->SetBinContent(i, value->GetBinContent(i) + value->GetBinContent(i - 1));
-            // Don't set lower limit of zero, or log scale won't work
-            value->GetYaxis()->SetRangeUser(std::numeric_limits<float>::epsilon(), 1);
-            canvas->cd();
-            gPad->SetLogy();
-            value->Draw("hist");
-            legend->Draw();
-        }
-    }
-    
-    for (const auto [key, canvas] : canvases)
-    {
-        std::string filename = "pdfs/c_" + type + "_" + key + ".pdf";
-        canvas->Print(filename.c_str());
-        delete canvas;
-    }
-    canvases.clear();
-    for (const auto [key, legend] : legends)
-    {
-        delete legend;
-    }
-    legends.clear();
+    // ATTN: Using global map variables
+    Accumulate(mapI2C);
+    Accumulate(mapI2P);
+    PlotMerge(mapI2C, "Completeness", isLog);
+    PlotMerge(mapI2P, "Purity", isLog);
 }
+
+void Plot(const bool isLog = false)
+{
+    // ATTN: Using global map variables - if running, run BEFORE PlotCumulative
+    PlotMerge(mapI2C, "Completeness", isLog);
+    PlotMerge(mapI2P, "Purity", isLog);
+}
+
